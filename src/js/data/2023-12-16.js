819a8fbfdadd5faefee77b295c497030ef4a3749
ShiguFan <153970073+ShiguFan@users.noreply.github.com>
2023-12-16 14:36:16 +0800

    Add files via upload

---
+++ src/js/data/2023-12-16.js
@@ -0,0 +1,228 @@
+dataSetVersion = "2023-12-16"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+  {
+    name: "Filter by Series Entry",
+    key: "games",
+    tooltip: "Check this to restrict to certain games.",
+    checked: false,
+    sub: [
+      { name: "20th Anniversary", key: "20th" },
+	  { name: "20th Anniversary Skins", key: "20thsk" },
+    ]
+  }
+];
+
+dataSet[dataSetVersion].characterData = [
+    {
+      name: "Arle Nadja",
+      img: "themainone.png",
+      opts: {
+        games: ["20th"]
+      }
+    },
+	{
+      name: "Rulue",
+      img: "mommy.png",
+      opts: {
+        games: ["20th"]
+      }
+    },
+	{
+      name: "Schezo Wegey",
+      img: "iwantyouguy.png",
+      opts: {
+        games: ["20th"]
+      }
+    },
+	{
+      name: "Carbuncle",
+      img: "gugugu.png",
+      opts: {
+        games: ["20th"]
+      }
+    },
+	{
+      name: "Satan(Dark Prince)",
+      img: "literallysatan.png",
+      opts: {
+        games: ["20th"]
+      }
+    },
+	{
+      name: "Draco Centauros",
+      img: "gaogao.png",
+      opts: {
+        games: ["20th"]
+      }
+    },
+	{
+      name: "Witch",
+      img: "witchmorelikebitch.png",
+      opts: {
+        games: ["20th"]
+      }
+    },
+	{
+      name: "Suketoudara",
+      img: "fwish.png",
+      opts: {
+        games: ["20th"]
+      }
+    },
+	{
+      name: "Amitie",
+      img: "whitegirl.png",
+      opts: {
+        games: ["20th"]
+      }
+    },
+	{
+      name: "Raffina",
+      img: "wagamuffin.png",
+      opts: {
+        games: ["20th"]
+      }
+    },
+	{
+      name: "Sig",
+      img: "myboy.png",
+      opts: {
+        games: ["20th"]
+      }
+    },
+	{
+      name: "Ms. Accord",
+      img: "allaccordingtoplan.png",
+      opts: {
+        games: ["20th"]
+      }
+    },
+	{
+      name: "Klug",
+      img: "nerdalert.png",
+      opts: {
+        games: ["20th"]
+      }
+    },
+	{
+      name: "Lidelle",
+      img: "cuteimpgirl.png",
+      opts: {
+        games: ["20th"]
+      }
+    },
+	{
+      name: "Lemres",
+      img: "candyman.png",
+      opts: {
+        games: ["20th"]
+      }
+    },
+	{
+      name: "Feli",
+      img: "morelikefelloffamirite.png",
+      opts: {
+        games: ["20th"]
+      }
+    },
+	{
+      name: "Ocean Prince",
+      img: "royalfwish.png",
+      opts: {
+        games: ["20th"]
+      }
+    },
+	{
+      name: "Yu & Rei",
+      img: "ghostofachance.png",
+      opts: {
+        games: ["20th"]
+      }
+    },
+	{
+      name: "Onion Pixie",
+      img: "oniondeeznu.png",
+      opts: {
+        games: ["20th"]
+      }
+    },
+	{
+      name: "Dongurigaeru",
+      img: "whitegirl.png",
+      opts: {
+        games: ["20th"]
+      }
+    },
+	{
+      name: "Ringo Ando",
+      img: "badapple.png",
+      opts: {
+        games: ["20th"]
+      }
+    },
+	{
+      name: "Maguro Sasaki",
+      img: "gamerboystar.png",
+      opts: {
+        games: ["20th"]
+      }
+    },
+	{
+      name: "Risukuma",
+      img: "abearorasquirrel.png",
+      opts: {
+        games: ["20th"]
+      }
+    },
+	{
+      name: "Ecolo",
+      img: "blobbyblob.png",
+      opts: {
+        games: ["20th"]
+      }
+    },
+	{
+      name: "Red Amitie",
+      img: "thesungodig.png",
+      opts: {
+        games: ["20thsk"]
+      }
+    },
+	{
+      name: "Black/Dark Sig",
+      img: "justaglimpseintomydarkreality.png",
+      opts: {
+        games: ["20thsk"]
+      }
+    },
+	{
+      name: "Yellow Satan(Onesie Prince)",
+      img: "literallysataninhispajamas.png",
+      opts: {
+        games: ["20thsk"]
+      }
+    },
+	{
+      name: "Unusual/Alter Ecolo",
+      img: "justadroolingscrunkle.png",
+      opts: {
+        games: ["20thsk"]
+      }
+    },
+	{
+      name: "White/Angelic Feli",
+      img: "icanbeyourangleoryuordevil.png",
+      opts: {
+        games: ["20thsk"]
+      }
+    },
+	{
+      name: "Strange/Possessed Klug",
+      img: "ayayay.png",
+      opts: {
+        games: ["20thsk"]
+      }
+    }
+];
